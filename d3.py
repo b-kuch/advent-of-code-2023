54b29d0fae6527b79afeed2103353ac3105a381f
k.bt@gmail.com <k.bt@gmail.com>
2023-12-03 13:16:19 +0100

    day three

---
+++ d3.py
@@ -0,0 +1,157 @@
+from itertools import chain, cycle
+from typing import Iterable, Iterator
+
+
+def part_one():
+    data = load_data()
+    return sum_part_numbers(data)
+
+
+def part_two():
+    data = load_data()
+    return sum_gear_ratios(data)
+
+
+def load_data():
+    data = []
+    with open('in3', 'r') as f:
+        for line in f:
+            data.append(line[:-1])
+    return data
+
+
+def tripletwise(iterable: Iterable):
+    if iterable is None:
+        return cycle([None])
+    a, b, c = chain([None], iterable), iterable, chain(iterable[1:], [None])
+    return zip(a, b, c)
+
+
+symbols = '=-@%/+&$*#'
+
+
+def find_symbols(prev: str, curr: str, nxt: str) -> Iterator[tuple[str, bool]]:
+    for window in zip(tripletwise(prev), tripletwise(curr), tripletwise(nxt)):
+        current = window[1][1]
+        symbol_in_window = False
+        for line in window:
+            if line is not None:
+                for char in line:
+                    if char is not None and char in symbols:
+                        symbol_in_window = True
+        yield current, symbol_in_window
+
+
+def sum_part_numbers(data: [str]) -> int:
+    part_numbers_sum = 0
+
+    current_number = []
+    is_current_number_part = False
+    for three_rows in tripletwise(data):
+        for current, symbol_nearby in find_symbols(*three_rows):
+            if current.isdigit():
+                current_number.append(current)
+                if symbol_nearby:
+                    is_current_number_part = True
+            else:
+                if is_current_number_part and len(current_number) > 0:
+                    part_numbers_sum += int(''.join(current_number))
+                current_number = []
+                is_current_number_part = False
+    return part_numbers_sum
+
+
+def find_nearby_numbers(prev, curr, nxt):
+    for window in zip(tripletwise(prev), tripletwise(curr), tripletwise(nxt)):
+        current = window[1][1]
+        if current == '*':
+            yield find_in_window(window)
+        yield []
+
+
+directions = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+]
+
+
+def find_in_window(window):
+    nearby_digits = []
+    for direction in directions:
+        if (
+                window[1 + direction[0]] is not None and
+                window[1 + direction[0]][1 + direction[1]] is not None and
+                window[1 + direction[0]][1 + direction[1]].isdigit()):
+            nearby_digits.append(direction)
+
+    if (-1, 0) in nearby_digits:
+        if (-1, -1) in nearby_digits:
+            nearby_digits.remove((-1, -1))
+        if (-1, 1) in nearby_digits:
+            nearby_digits.remove((-1, 1))
+    if (1, 0) in nearby_digits:
+        if (1, -1) in nearby_digits:
+            nearby_digits.remove((1, -1))
+        if (1, 1) in nearby_digits:
+            nearby_digits.remove((1, 1))
+
+    return nearby_digits
+
+
+def my_enumerate(x):
+    for y in enumerate(x):
+        print(f'in enumerate {y}')
+        yield y
+
+
+def scan_rows(row_number, prv_row, cur_row, nxt_row):
+    locations = []
+    for column_number, (prv, cur, nxt) in enumerate(
+            zip(tripletwise(prv_row), tripletwise(cur_row), tripletwise(nxt_row))):
+        if cur[1] == '*':
+            gear_numbers = find_in_window([prv, cur, nxt])
+            if len(gear_numbers) == 2:
+                locations.append(((row_number, column_number), gear_numbers))
+            if len(gear_numbers) > 2:
+                raise ValueError('EEEEEE')
+    return locations
+
+
+def find_gears(data: [str]) -> list:
+    gears = []
+    for idx, three_rows in enumerate(tripletwise(data)):
+        new_gears = scan_rows(idx, *three_rows)
+        gears.extend(new_gears)
+    return gears
+
+
+def sum_gear_ratios(data: [str]) -> int:
+    gears = find_gears(data)
+
+    gear_ratios_sum = 0
+    for gear in gears:
+        gear_ratios_sum += find_ratio(data, gear)
+    return gear_ratios_sum
+
+
+def find_ratio(data: [str], gear):
+    gear_position, dirs = gear
+    ratio = 1
+    for direction in dirs:
+        number_line, number_start = gear_position[0] + direction[0], gear_position[1] + direction[1]
+        number_end = gear_position[1] + direction[1]
+
+        while number_start > 0 and data[number_line][number_start - 1].isdigit():
+            number_start -= 1
+
+        while number_end + 1 < len(data[0]) and data[number_line][number_end + 1].isdigit():
+            number_end += 1
+
+        ratio *= int(data[number_line][number_start:number_end + 1])
+    return ratio
---
+++ test_d3.py
@@ -0,0 +1,118 @@
+from d3 import find_gears, part_one, part_two, sum_gear_ratios, sum_part_numbers, find_symbols, tripletwise
+def test_find_gear_ratios():
+    data = (
+        '12...',
+        '..*..',
+        '...32'
+    )
+    assert [((1, 2), [(-1, -1), (1, 1)])] == find_gears(data)
+    data = (
+        '123..',
+        '..*..',
+        '...32'
+    )
+    assert [((1, 2), [(-1, 0), (1, 1)])] == find_gears(data)
+
+
+def test_part_one():
+    assert 520135 == part_one()
+
+
+def test_part_two():
+    assert None is part_two()
+
+
+def test_failing_find_ratios():
+    data = """.........798...145.........629....579.....455.....................130.............243.................154........167........................
+............*.....*...........*...&...179.*........737...194.........*854........./...........52..560*............................699...&...
+........459..489.817........880.........*..996........*....*........................................................................*.36....
+""".split()
+    assert ((1, 12), [(-1, -1), (1, 1)]) in find_gears(data)
+    assert ((1, 18), [(-1, -1), (1, 0)]) in find_gears(data)
+    assert ((1, 30), [(-1, -1), (1, 0)]) in find_gears(data)
+
+
+def test_example_input_for_part_two():
+    data = """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..""".split('\n')
+    assert 467835 == sum_gear_ratios(data)
+
+
+def test_example_input_for_part_two_short():
+    data = """467..114..
+...*......
+..35..633.""".split('\n')
+    assert 16345 == sum_gear_ratios(data)
+
+
+def test_example_input():
+    data = """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..""".split('\n')
+    assert 4361 == sum_part_numbers(data)
+
+
+def test_sum_part_numbers():
+    assert 123 == sum_part_numbers(['*123...321'])
+
+
+def test_find_symbols():
+    data = [
+        '....',
+        'ZXC.',
+        '...*'
+    ]
+    iterator = find_symbols(*data)
+    current, symbol_nearby = next(iterator)
+    assert current == 'Z'
+    assert not symbol_nearby
+    current, symbol_nearby = next(iterator)
+    assert current == 'X'
+    assert not symbol_nearby
+
+    current, symbol_nearby = next(iterator)
+    assert current == 'C'
+    assert symbol_nearby
+
+
+def test_tripletwise():
+    data = 'abc'
+    trio = tripletwise(data)
+    first, second, third = next(trio)
+    assert first is None
+    assert second == 'a'
+    assert third == 'b'
+    first, second, third = next(trio)
+    assert first == 'a'
+    assert second == 'b'
+    assert third == 'c'
+    first, second, third = next(trio)
+    assert third is None
+
+    try:
+        next(trio)
+        raise AssertionError('StopIteration should have happened')
+    except StopIteration:
+        pass
+
+
+def test_find_symbols_with_none():
+    data = (None, 'abcdefg', None)
+    iterator = find_symbols(*data)
+    for char in data[1]:
+        assert next(iterator) == (char, False)
